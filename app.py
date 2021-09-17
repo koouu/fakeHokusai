a644bb79aed82c833e6cf2d9ef996e6697864b8a
koouu <nkouta0420@gmail.com>
2021-09-17 13:25:22 +0900

    cv

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
 #googledrivedownloader==0.4
 #from google_drive_downloader import GoogleDriveDownloader as gdd
 """
