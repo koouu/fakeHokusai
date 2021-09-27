b7ac29365489cb41a97dc73b1d623d3f70a8ecd1
koouu <nkouta0420@gmail.com>
2021-09-27 23:59:38 +0900

    update

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-#googledrivedownloader==0.4
-#from google_drive_downloader import GoogleDriveDownloader as gdd
 
 from UGATIT import UGATIT
 
